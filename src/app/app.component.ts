caad373060e9e4a24ce958ab93d7c3664fc766d0
Andrei Ionita <ionita_andrei_2002@yahoo.com>
2024-02-02 22:41:47 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ParentComponent} from "./A/parent/parent.component";
+import {ParentBComponent} from "./B/parent-b/parent-b.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, ParentComponent, ParentBComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
